417d4beb815a3d71f5d753dfca0570711f940e0c
Tyler Mayes <timmymayes@gmail.com>
2022-07-19 17:08:29 -0700

    Setup eventlisteners for the buttons and evaluate a single round
    The output is still console log but it has correct verbage.

--- rps.js
+++ rps.js
@@ -1,8 +1,22 @@
-// need a function computerPlay that will randomly return rock paper or scissors. This is the computers player
+// setup event listeners
 
 const ROCK = 3;
 const SCISSOORS = 2;
 const PAPER = 1;
+
+function getPlayerSelection(e) {
+  let playerWeapon = setWeapon(e.target.id);
+  let computerWeapon = computerPlay();
+  printResult(
+    executeRound(playerWeapon, computerWeapon),
+    playerWeapon,
+    computerWeapon
+  );
+  console.log(executeRound());
+}
+
+const buttons = document.querySelectorAll(".weapon");
+buttons.forEach((btn) => btn.addEventListener("click", getPlayerSelection));
 
 function computerPlay() {
   let min = Math.ceil(1);
@@ -10,7 +24,7 @@ function computerPlay() {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function playRound(playerSelection, computerSelection) {
+function executeRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return false; // player didn't win
   }
@@ -81,36 +95,4 @@ function printResult(isPlayerWinner, playerWeapon, computerWeapon) {
   }
 }
 
-function gameInConsole() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let playerChoice = 0;
-  let computerChoice;
-  for (var i = 0; i < 5; i++) {
-    while (playerChoice === 0) {
-      playerChoice = setWeapon(
-        prompt("Select your weapon: Rock, Paper or Scissors")
-      );
-      if (playerChoice === 0) {
-        alert("You cannot use that weapon in this fight!");
-      }
-    }
-    computerChoice = computerPlay();
-    let didPlayerWin = playRound(playerChoice, computerChoice);
-    if (didPlayerWin) {
-      playerScore += 1;
-    } else if (playerChoice != computerChoice) {
-      computerScore += 1;
-    }
-    printResult(didPlayerWin, playerChoice, computerChoice);
-    playerChoice = 0; // reset the while.
-  }
-  console.log(
-    "And the winner is....." +
-      (playerScore >= computerScore ? "The Hooman" : "The COMPUTAR") +
-      " with a score of " +
-      (playerScore >= computerScore ? playerScore : computerScore) +
-      " to " +
-      (playerScore >= computerScore ? computerScore : playerScore)
-  );
-}
+// computerChoice = computerPlay();
